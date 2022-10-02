3eb1787bdddefa7f4ffb694f6f3c4dfafd0835dc
Daksh Sharma <Daksh.Sharma@dpworld.com>
2022-10-02 18:23:42 +0530

    first commit

---
+++ auth/src/models/user.ts
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";
+import { Password } from "../services/password";
+// interface describing properties to create new user
+interface UserAttrs{
+    email:string,
+    password:string
+}
+
+// interface describing properties that a User model has
+interface UserModel extends mongoose.Model<UserDoc>{
+    build(attrs:UserAttrs):UserDoc;
+}
+
+// interface describing a user document
+interface UserDoc extends mongoose.Document{
+    email:string,
+    password:string, 
+}
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+},{
+    toJSON:{
+        transform(doc:any,ret:any){ 
+            ret.id = ret._id
+            delete ret.password
+            delete ret._id
+            delete ret.__v
+        }
+    }
+})
+
+userSchema.pre('save', async function(done){
+    if(this.isModified('password')){
+        const hashed = await Password.toHash(this.get('password'))
+        this.set('password',hashed)
+    }
+    done()
+})
+
+userSchema.statics.build = (attrs:UserAttrs)=>{
+    return new User(attrs)
+}
+
+const User = mongoose.model<UserDoc,UserModel>('User',userSchema)
+
+export { User }
